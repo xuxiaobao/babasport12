cd20d648c33ea52dd1de480c4a4ec1afc37ccff7
xuxiaobao <2269649004@qq.com>
2017-02-25 21:15:37 +0800

    2017-2-25 21:15

--- src/main/java/cn/itcast/core/controller/admin/BrandController.java
+++ src/main/java/cn/itcast/core/controller/admin/BrandController.java
@@ -3,6 +3,7 @@ package cn.itcast.core.controller.admin;
 import cn.itcast.core.controller.BaseController;
 import cn.itcast.core.service.product.BrandService;
 import cn.itcast.core.web.pojo.WebParam;
+import cn.itcast.core.web.util.HrefParamUnion;
 import cn.itcast.page.Pagination;
 import cn.itcast.validator.Validators;
 import cn.itcast.validator.annotation.Params;
@@ -17,26 +18,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by xxb on 2017/2/24.
  */
 @Controller
+@RequestMapping("/brand")
 public class BrandController extends BaseController{
 
     @Autowired
     private BrandService brandService;
 
-    @RequestMapping(value = "/brand/list.do")
+    @RequestMapping(value = "/toAdd.do")
+    public String toAdd() {
+
+        return "brand/add";
+    }
+
     @Valid(value = {
-            @Params(name = "isDisplay", type = Integer.class, validator = Validators.COMMON.REQUIRED),
-            @Params(name = "name", type = String.class,validator = Validators.COMMON.REQUIRED)/*,
-            @Params(name = "page", type = Integer.class, validator = Validators.COMMON.REQUIRED),
-            @Params(name = "rows", type = Integer.class, validator = Validators.COMMON.REQUIRED)*/
+            @Params(name = "isDisplay", type = Integer.class, validator = Validators.NONE),
+            @Params(name = "name", type = String.class,validator = Validators.NONE),
+            @Params(name = "pageNo", type = Integer.class, validator = Validators.NONE),
+            @Params(name = "pageSize", type = Integer.class, validator = Validators.NONE)
     })
+    @RequestMapping(value = "/list.do")
     public String brandList(ModelMap model) {
+        StringBuilder params = new StringBuilder();
         WebParam webParam = getWebParam();
-        webParam.put("page",1);
-        webParam.put("rows",5);
-        Pagination list = brandService.getBrandListWithPage(webParam);
-        model.addAttribute("pagination",list);
-        model.addAttribute("name",webParam.get("name"));
-        model.addAttribute("isDisplay",webParam.get("isDisplay"));
+        /*设置分页参数*/
+        initPagination();
+        Pagination pagination = brandService.getBrandListWithPage(webParam);
+        params = HrefParamUnion.paramsUnion(webParam,"name","isDisplay");
+        pagination.pageView("/brand/list.do",params.toString());
+        model.addAttribute("pagination",pagination);
+        model.addAttribute("pams",webParam);
         return "brand/list";
     }
 }
